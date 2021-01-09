1e6a92dd0b04a99382fadd3d7f22f3422b52c85e
Nathanael <nathanael.vd@gmail.com>
2021-01-10 02:36:35 +0700

    Stack dan Align Widget

---
+++ stack_align_widget/lib/main.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  final List<Widget> data = <Widget>[];
+
+  MyApp() {
+    for (var i = 0; i < 100; i++) {
+      data.add(
+          Text('Data Aku Suka Susunya Ke-$i', style: TextStyle(fontSize: 25)));
+    }
+  }
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text('Stack dan Align Widget'),
+        backgroundColor: Colors.pink[100],
+      ),
+      body: Stack(
+        children: <Widget>[
+          // background
+          Column(
+            children: <Widget>[
+              Flexible(
+                  flex: 1,
+                  child: Row(
+                    children: <Widget>[
+                      Flexible(flex: 1, child: Container(color: Colors.red)),
+                      Flexible(flex: 1, child: Container(color: Colors.green)),
+                    ],
+                  )),
+              Flexible(
+                  flex: 1,
+                  child: Row(
+                    children: <Widget>[
+                      Flexible(flex: 1, child: Container(color: Colors.green)),
+                      Flexible(flex: 1, child: Container(color: Colors.red))
+                    ],
+                  ))
+            ],
+          ),
+          // listView
+
+          ListView(
+            children: data,
+          ),
+          // button
+          Align(
+            alignment: Alignment(0.75, 0.90), // costum dengan Alignment(x,y)
+            child: RaisedButton(
+              child: Text(
+                'Tombol',
+                style: TextStyle(color: Colors.white),
+              ),
+              color: Colors.black,
+              onPressed: () {},
+            ),
+          ),
+        ],
+      ),
+    ));
+  }
+}
