fd0ad2d62d8d93cdcd41a0445b0530347cf5e087
Nathanael <nathanael.vd@gmail.com>
2021-01-09 21:03:59 +0700

    Anonymous Method

--- anonymous_method/lib/main.dart
+++ anonymous_method/lib/main.dart
@@ -2,18 +2,32 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(
-        title: Text('Anonymous Method'),
-        backgroundColor: Colors.pink[100],
+      home: Scaffold(
+        appBar: AppBar(
+            title: Text('Anonymous Method'), backgroundColor: Colors.pink[100]),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text('Hello World'),
+              RaisedButton(
+                child: Text('Tekan Aku'),
+                color: Colors.cyan,
+                onPressed: null,
+              )
+            ],
+          ),
+        ),
       ),
-      body: Center(
-        child: Text('Hello Claresta'),
-      ),
-    ));
+    );
   }
 }
