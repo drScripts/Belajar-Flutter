aadd8ecb7c9b1b752bebbb4dffc102b075bcef60
Nathanael <nathanael.vd@gmail.com>
2021-03-14 13:13:13 +0700

    bloc test 2

--- bloc_test_2/lib/list_product/product.dart
+++ bloc_test_2/lib/list_product/product.dart
@@ -4,7 +4,7 @@ class Product extends Equatable {
   final String name;
   final int price;
 
-  Product({this.name, this.price});
+  const Product({this.name, this.price});
   @override
   List<Object> get props => [name, price];
 }
